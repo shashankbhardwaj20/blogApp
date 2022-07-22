e76b25eab456ae7a8cc3837b665e3139c8c90e6c
shashank00bhardwaj <98531138+shashank00bhardwaj@users.noreply.github.com>
2022-07-22 22:42:52 +0530

    version2

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ const User = mongoose.model('user',{
   password : String
 });
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const date = new Date();
 options={
   weekday:"long",
@@ -125,16 +122,8 @@ app.get('/logout',function(req,res){
 });
 
 
-app.get("/post/:postID",function(req,res){  //express routing parameters
-//  const requestedTitle=_.lowerCase(req.params.postID);
-//   for(var i=0;i<posts.length;++i){
-//     const storedTitle = _.lowerCase(posts[i].postTitle);
-//     if(requestedTitle===storedTitle){
-//       res.render("post",{postTitle:storedTitle,postContent:posts[i].postContent});
-//       break;
-//     }
-//  }
-
+app.get("/post/:postID",function(req,res){  
+  
   Post.findById(req.params.postID,function(err,postFound){
     if(!err) res.render("post",
     {postTitle:postFound.postTitle,
